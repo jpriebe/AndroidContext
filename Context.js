32b6b77bff0205429bfe4b32ad9fedaacc39d581
Jason Priebe <jason.priebe@gmail.com>
2014-11-10 12:43:54 -0500

    modified to manage the activities at the window level
    Timan Rebel has discovered the best technique I've seen for accurate handling of paused/resumed events in Titanium on android.  It requires that the developer use discipline when creating and opening windows.  The technique demands that the app track the start/stop events on every single activity in the app.  So every time you create a window, you have to let this centralized system know about it so that it can monitor the start/stop events.  You also have to give every activity a unique name.
    
    My changes are trying to make that work a little bit easier.  The developer doesn't have to name the activities; the Context library will handle that for you internally.  And the developer doesn't even have to think about the activity associated with each window; he just calls Context.track(), passing the window object.

--- Context.js
+++ Context.js
@@ -1,28 +1,51 @@
-module.exports = {
-	on: function(name, activity) {
-		activity.onStart = function() {
-			if (activeActivity == name) {
-				Ti.App.fireEvent('resumed');
-			}
+var _activeActivity;
 
-			activeActivity = name;
-		};
+function generate_guid ()
+{
+    var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+    
+    return guid;
+}
 
-        activity.onStop = function() {
-			if (activeActivity == name) {
-				Ti.App.fireEvent('paused');
-			}
-		};
-	},
+function on_open (w) 
+{
+    var name = w.context_guid;
+    var activity = w.activity;
+    
+    activity.onStart = function() {
+        if (_activeActivity == name) {
+            Ti.App.fireEvent('resumed');
+        }
 
-	off: function(activity) {
-        activity.onStart = null;
-		activity.onStop = null;
-	}
+        _activeActivity = name;
+    };
+
+    activity.onStop = function() {
+        if (_activeActivity == name) {
+            Ti.App.fireEvent('paused');
+        }
+    };
+}
+
+function on_close (w) 
+{
+    var activity = w.activity;
+
+    activity.onStart = null;
+    activity.onStop = null;
+}
+
+function Context ()
+{
+}
+
+Context.track = function (win) {
+    win.context_guid = generate_guid ();
+    win.addEventListener ('open', function (e) { on_open (win); });
+    win.addEventListener ('close', function (e) { on_close (win); });
 };
 
-/**
- * @property {String} Current active Activity's name
- * @private
- */
-var activeActivity;
+module.exports = Context;
